d407df6d345beab81d59eea833cf06014b2ff459
Jefferson Chaves <jefferson.chaves@ifpr.edu.br>
2026-02-26 16:01:38 -0300

    feat: Controller, Router and Autoload add

--- app/controllers/PlayerController.php
+++ app/controllers/PlayerController.php
@@ -1,17 +1,35 @@
 <?php 
+namespace app\controllers;
 
-class PlayerController {
+use app\core\Controller;
+use DateTimeImmutable;
+
+class PlayerController extends Controller {
 
     public function __construct()
     {
     }
 
     public function index() {
-        $list = ['Neymar', 'Bebeto', 'Gabigol', 'Cebola'];
+        $data['list'] = ['Neymar', 'Bebeto', 'Gabigol', 'Cebola'];
 
-        require_once __DIR__ . '/../views/players/players_list.php';
+        $this->view('players/players_list', $data);
+    }
+
+    public function jogador(){
+
+        $data['date'] = new DateTimeImmutable();
+        $data['jogador'] = ['Neymar', '36', 'Meia Atacante']; 
+
+        $this->view('players/players_show', $data);
 
     }
+
+    public function testRedirect(){
+        $this->redirect("http://google.com");
+    }
+
+
 
 
 } 
